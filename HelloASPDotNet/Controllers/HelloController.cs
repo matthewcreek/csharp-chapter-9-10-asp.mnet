60f34a76ac4383b05991929b97deb2b892d94498
Matthew Creek <matthew.creek27@gmail.com>
2024-05-28 10:12:12 -0500

    chapter reading complete

---
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloASPDotNet.Controllers;
+
+[Route("/helloworld")]
+public class HelloController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        string html = "<form method='post' action='/helloworld'>" +
+            "<input type='text' name='name' />" +
+            "<input type='submit' value='Greet Me!' />" +
+            "</form>";
+
+        return Content(html, "text/html");
+    }
+
+    [HttpGet("welcome/{name?}")]
+    [HttpPost("welcome")]
+    public IActionResult Welcome(string name = "World")
+    {
+        return Content($"Welcome to my app, {name}!", "text/html");
+    }
+}
