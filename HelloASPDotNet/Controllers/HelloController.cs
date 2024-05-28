5e624adffe2d280637d28d47b4171bb218494e0d
Matthew Creek <matthew.creek27@gmail.com>
2024-05-28 10:42:15 -0500

    functioning code

--- HelloASPDotNet/Controllers/HelloController.cs
+++ HelloASPDotNet/Controllers/HelloController.cs
@@ -8,12 +8,14 @@ public class HelloController : Controller
     [HttpGet]
     public IActionResult Index()
     {
-        string html = "<form method='post'>" +
+        string html = "<form method='post' action='/helloworld/welcome'>" +
             "<input type='text' name='name' />" +
             "<select name='language'>" + 
             "<option value='english'>English</option>" +
             "<option value='spanish'>Spanish</option>" +
             "<option value='french'>French</option>" +
+            "<option value='bosniam'>Bosnian</option>" +
+            "<option value='vietnamese'>Vietnamese</option>" +
             "</select>" +
             "<input type='submit' value='Greet Me!' />" +
             "</form>";
@@ -21,10 +23,41 @@ public class HelloController : Controller
         return Content(html, "text/html");
     }
 
+    // [HttpGet("welcome/{name?}")]
+    // [HttpPost("welcome")]
+    // public IActionResult Welcome(string name = "World")
+    // {
+    //     return Content($"Welcome to my app, {name}!", "text/html");
+    // }
+
     [HttpGet("welcome/{name?}")]
     [HttpPost("welcome")]
-    public IActionResult Welcome(string name = "World")
+    public IActionResult Display(string name = "World", string language = "english")
     {
-        return Content($"Welcome to my app, {name}!", "text/html");
+        return Content(CreateMessage(name, language));
+    }
+
+    public static string CreateMessage(string name, string language) 
+    {
+        string helloTranslation = "";
+        switch (language)
+        {
+            case "french":
+                helloTranslation = "Bonjour ";
+                break;
+             case "spanish":
+                 helloTranslation = "Hola ";
+                 break;
+             case "bosnian":
+                 helloTranslation = "Zdravo ";
+                 break;
+             case "vietnamese":
+                 helloTranslation = "Xin Chao ";
+                 break;
+             case "english":
+                 helloTranslation = "Hello ";
+                 break;
+        }
+        return helloTranslation + name;
     }
 }
--- HelloASPDotNet/obj/Debug/net6.0/HelloASPDotNet.AssemblyInfo.cs
+++ HelloASPDotNet/obj/Debug/net6.0/HelloASPDotNet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloASPDotNet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f34a76ac4383b05991929b97deb2b892d94498")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94760e0f42df6929eab1f1d9ccfccca88798b45")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloASPDotNet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloASPDotNet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
